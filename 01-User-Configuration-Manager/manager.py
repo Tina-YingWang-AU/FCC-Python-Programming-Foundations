3f73d85f2c4e93975ebaec4ac765d8db6c88eecb
Tina (Ying) Wang <57971822+Tina-YingWang-AU@users.noreply.github.com>
2026-02-24 16:14:01 +1100

    Create manager.py

---
+++ 01-User-Configuration-Manager/manager.py
@@ -0,0 +1,65 @@
+# build a User Configuration Manager that allows users to manage their settings such as theme, language, and notifications. You will implement functions to add, update, delete, and view user settings.
+
+from functools import update_wrapper
+
+def add_setting(dictionary_setting, my_tuple):
+    lower_my_tuple = tuple(s.lower() for s in my_tuple)
+    check_add = []
+    for key in dictionary_setting:
+        if key.lower() == lower_my_tuple[0]:
+            check_add.append(False)
+            return f"Setting '{lower_my_tuple[0]}' already exists! Cannot add a new setting with this name."
+
+        else:
+            check_add.append(True)
+
+    if all(check_add):
+        dictionary_setting.update({lower_my_tuple[0]: lower_my_tuple[1]})
+        # print(dictionary_setting)
+        return f"Setting '{lower_my_tuple[0]}' added with value '{lower_my_tuple[1]}' successfully!"
+
+
+def update_setting(dictionary_setting, my_tuple):
+    lower_my_tuple = tuple(s.lower() for s in my_tuple)
+    check_update = []
+    for key in dictionary_setting:
+        if key.lower() == lower_my_tuple[0]:
+            dictionary_setting.update({lower_my_tuple[0]: lower_my_tuple[1]})
+            # print(dictionary_setting)
+            return f"Setting '{lower_my_tuple[0]}' updated to '{lower_my_tuple[1]}' successfully!"
+        else:
+            check_update.append(False)
+
+
+    if not all(check_update):
+        # print(dictionary_setting)
+        return f"Setting '{lower_my_tuple[0]}' does not exist! Cannot update a non-existing setting."
+
+def delete_setting(dictionary_setting, my_key):
+    lower_my_key = my_key.lower()
+
+    check_delete = []
+    for key in dictionary_setting:
+        if key.lower() == lower_my_key:
+            dictionary_setting.pop(key)
+            # print(dictionary_setting)
+            return f"Setting '{lower_my_key}' deleted successfully!"
+        else:
+            check_delete.append(False)
+
+    if not all(check_delete):
+        return "Setting not found!"
+
+
+def view_settings(dictionary_setting):
+    view_result = "Current User Settings:\n"
+    if dictionary_setting == {}:
+        return "No settings available."
+    else:
+        for key, value in dictionary_setting.items():
+            view_result += f"{key.capitalize()}: {value}\n"
+        return view_result
+
+
+result = view_settings({'theme': 'dark', 'notifications': 'enabled', 'volume': 'high'})
+print(result)
